ef9d8f40e137ea8dc086e66ee67036f4236d3ad8
Gordon Hogenson <ghogen@microsoft.com>
2018-08-01 12:01:01 -0700

    Add hook for edge call

---
+++ RepoManager/lucenesearch/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuceneSearch
+{
+    public static class Startup
+    {
+
+        public static async Task<object> Invoke(object input)
+        {
+            return await Task.Run(() => DocSearcher.Search((string)input));
+        }
+    }
+}
